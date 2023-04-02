c9f47e8913a149d8ac16e78ff4da4ac4351c63a7
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2023-04-01 23:50:34 -0400

    Button Created

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -33,7 +35,9 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier
     .wrapContentSize(Alignment.Center)){
     Column(modifier = modifier,
     horizontalAlignment = Alignment.CenterHorizontally) {
-
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
     }
 
 }
